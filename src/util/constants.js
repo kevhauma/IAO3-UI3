8377b0b324673e0f3d70dd118f9351a42d8aa7c8
kevhauma <hauman.kevin@hotmail.com>
2020-01-04 13:51:36 +0100

    code for remote backend in place

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,3 +1,5 @@
-export const URI_PATH =  "http://192.168.0.15:3000"
-export const SOCKET_PATH =  "http://192.168.0.15:3001/socket"
+//const URL = "http://81.83.89.2"
+const URL = "http://localhost"
+export const URI_PATH =  `${URL}:3000`
+export const SOCKET_PATH =  `${URL}:3001/socket`
 export const allowedActions = ["eten","wassen","omkeren"]
